4f0aec867c8225e568259a0a07a6ac661e8ea929
Marco Miralles <marco.miralles@gmail.com>
2025-01-03 22:54:10 +0900

    refactor screen shader, add new getOcclusionFactor function

--- res/shaders/ScreenShader.glsl
+++ res/shaders/ScreenShader.glsl
@@ -50,6 +50,26 @@ const float offset = 1.0 / 600.0;
 
 vec3 lightColor = vec3(1.0, 1.0, 1.0);
 
+float getOcclusionFactor(vec4 worldSpacePos, float diffuseFactor)
+{
+    vec4 shadowFragPos = lightMVP * worldSpacePos;
+    shadowFragPos.xyz /= shadowFragPos.w;
+    shadowFragPos.xyz = (shadowFragPos.xyz + 1.0)/ 2.0;
+    vec2 shadowMapUV = shadowFragPos.xy;
+
+    // vec2 shadowMapUV = (shadowFragPos.xy + 1.0)/ 2.0; ????
+
+    if (shadowMapUV.x < 0.0 || shadowMapUV.x > 1.0 || shadowMapUV.y < 0.0 || shadowMapUV.y > 1.0) {
+        return 0.0;
+    }
+
+    float shadowDepth = texture(shadowTexture, shadowMapUV).r;
+
+    float bias = mix(0.0005, 0.0, diffuseFactor);
+    
+    return shadowDepth < shadowFragPos.z - bias ? 1.0 : 0.0;
+}
+
 void main()
 {
     vec3 FragPos = texture(gPosition, texCoords).rgb;
@@ -66,47 +86,17 @@ void main()
     vec4 worldSpacePos = inverseMVP * clipSpacePos;
     worldSpacePos /= worldSpacePos.w;
 
-    vec4 shadowFragPos = lightMVP * worldSpacePos;
-    shadowFragPos.xyz /= shadowFragPos.w;
-    shadowFragPos.xyz = (shadowFragPos.xyz + 1.0)/ 2.0;
-
-    float shadowDepth = texture(shadowTexture, shadowFragPos.xy).r;
-
     float AmbientOcclusion = pow(texture(ssaoTexture, texCoords).r, 2);
 
     vec3 lighting = Diffuse * 0.5 * (occlusion ? AmbientOcclusion : 1.0); //ambient
 
     vec3 lightDir = normalize(lightPos - FragPos);
-    vec3 diffuse = max(dot(Normal, lightDir), 0.0) * Diffuse * lightColor;
-
-    // float kernel[9] = float[](
-    //     -1, -1, -1,
-    //     -1,  9, -1,
-    //     -1, -1, -1
-    // );
-    // float kernel[9] = float[](
-    //     1.0/16, 2.0/16, 1.0/16,
-    //     2.0/16, 4.0/16, 2.0/16,
-    //     1.0/16, 2.0/16, 1.0/16
-    // );
-    
-    // vec4 col = vec4(0.0);
-    // col += kernel[0] * texture(gAlbedo, texCoords + vec2(-offset, offset )); // top left
-    // col += kernel[1] * texture(gAlbedo, texCoords + vec2(0.0f,    offset )); // top center
-    // col += kernel[2] * texture(gAlbedo, texCoords + vec2(offset,  offset )); // top right
-    // col += kernel[3] * texture(gAlbedo, texCoords + vec2(-offset, 0.0f   )); // left
-    // col += kernel[4] * texture(gAlbedo, texCoords + vec2(0.0f,    0.0f   )); // center
-    // col += kernel[5] * texture(gAlbedo, texCoords + vec2(offset,  0.0f   )); // right
-    // col += kernel[6] * texture(gAlbedo, texCoords + vec2(-offset, -offset)); // bottom left
-    // col += kernel[7] * texture(gAlbedo, texCoords + vec2(0.0f,    -offset)); // bottom center
-    // col += kernel[8] * texture(gAlbedo, texCoords + vec2(offset,  -offset)); // bottom right
-    // fragColor = col;
+    float diffuseFactor = max(dot(Normal, lightDir), 0.0);
+    vec3 diffuse = diffuseFactor * Diffuse * lightColor;
 
     lighting += diffuse;
 
-    if (shadowDepth < shadowFragPos.z - 0.00001)
-    {
-        lighting /= 2.0;
-    }
+    lighting -= lighting * 0.5 * getOcclusionFactor(worldSpacePos, diffuseFactor);
+
     fragColor = vec4(lighting, 1.0);
 }
